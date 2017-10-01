bb5d9f1d3e60b9d2878751867ba6195b69ad5b2f
Keith <keithtmccartney@hotmail.com>
2017-10-01 22:27:16 +0100

    Adding MVC to the Service Collection.

--- ASPNETCOREDEMO/ASPNETCOREDEMO/Startup.cs
+++ ASPNETCOREDEMO/ASPNETCOREDEMO/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System.IO;
 
 namespace ASPNETCOREDEMO
 {
@@ -16,12 +17,13 @@ namespace ASPNETCOREDEMO
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
+            /*loggerFactory.AddConsole();
 
             if (env.IsDevelopment())
             {
@@ -31,7 +33,23 @@ namespace ASPNETCOREDEMO
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
+            });*/
+
+            app.Use(async (context, next) =>
+            {
+                await next();
+
+                if (context.Response.StatusCode == 404 && !Path.HasExtension(context.Request.Path.Value) && !context.Request.Path.Value.StartsWith("/api/"))
+                {
+                    context.Request.Path = "./src/index.html";
+
+                    await next();
+                }
             });
+
+            app.UseMvcWithDefaultRoute();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
     }
 }
